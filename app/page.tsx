6a472c6a9670070656ef9a77ae5b4a6db13ac9b1
jonathanfernandezfm <jonathanfernandezfm@gmail.com>
2023-10-18 23:16:20 +0300

    Removed redirection

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    redirect("/api/auth/signin");
+    // redirect("/api/auth/signin");
   }
 
   return (
